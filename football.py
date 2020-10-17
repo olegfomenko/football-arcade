2a6140ed0330cf7604b0bb78c8716ae762805154
olegfomenko <oleg.fomenko2002@gmail.com>
2020-10-18 02:49:42 +0300

    adding players. now all sizes depends on field scale

--- football.py
+++ football.py
@@ -1,38 +1,129 @@
 import arcade
 
 
+class GameObject:
+    def __init__(self, sprite: arcade.Sprite, default_x, default_y, scale):
+        self.sprite = sprite
+
+        self.width = self.sprite.width * scale
+        self.height = self.sprite.height * scale
+
+        self.sprite.scale = scale
+        self.sprite.color = arcade.color.GOLD
+        self.sprite.set_position(default_x, default_y)
+
+        self.default_x = self.x = default_x
+        self.default_y = self.y = default_y
+
+    def to_default(self):
+        self.x = self.default_x
+        self.y = self.default_y
+        self.sprite.set_position(self.x, self.y)
+
+    def draw(self):
+        self.sprite.draw()
+
+    def update(self, delta_time):
+        self.sprite.set_position(self.x, self.y)
+
+
+class Ball(GameObject):
+    ACCELERATION = 50
+    EPS = 0e-9
+
+    def __init__(self, sprite, x, y, scale):
+        super().__init__(sprite, x, y, scale)
+
+        self.speed_x = 0
+        self.speed_y = 0
+
+    def update(self, delta_time):
+        if self.speed_x != 0 or self.speed_y != 0:
+            self.x += self.speed_x * delta_time
+            self.y += self.speed_y * delta_time
+
+            if self.speed_x > 0:
+                self.speed_x -= Ball.ACCELERATION * delta_time
+            else:
+                self.speed_x += Ball.ACCELERATION * delta_time
+
+            if self.speed_y > 0:
+                self.speed_y -= Ball.ACCELERATION * delta_time
+            else:
+                self.speed_y += Ball.ACCELERATION * delta_time
+
+            if abs(self.speed_x) < Ball.EPS:
+                self.speed_x = 0
+
+            if abs(self.speed_y) < Ball.EPS:
+                self.speed_y = 0
+
+        super().update(delta_time)
+
+    def change_speed(self, speed_x, speed_y):
+        self.speed_x = speed_x * 1.5
+        self.speed_y = speed_y * 1.5
+
+
+class Player(GameObject):
+    def __init__(self, sprite, x, y, scale):
+        super().__init__(sprite, x, y, scale)
+
+        self.speed_x = 0
+        self.speed_y = 0
+
+    def update(self, delta_time):
+        if self.speed_x != 0 or self.speed_y != 0:
+            self.x += self.speed_x * delta_time
+            self.y += self.speed_y * delta_time
+
+        super().update(delta_time)
+
+
 class Field:
-    def __init__(self, x, y, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self, x, y, scale):
+        self.scale = scale
+
+        self.border_list = arcade.SpriteList()
+        self.field = arcade.Sprite("sprite/field_texture.png")
+
+        self.width = self.field.width * scale
+        self.height = self.field.height * scale
+
+        self.field.scale = scale
 
         self.x = x
         self.y = y
 
-        self.borderList = arcade.SpriteList()
-        self.field = arcade.Sprite("sprite/field_texture.png")
+        self.ball = Ball(arcade.Sprite("sprite/ball.png"), x, y, scale)
+
+        self.player = Player(arcade.Sprite("sprite/red_player.png"), x - 100, y, scale)
 
     def __init_borders(self):
         top_border = arcade.Sprite("sprite/top_border.png")
-        top_border.width = self.width
-        top_border.height = self.height / 2.5
+        top_border.scale = self.scale
         top_border.set_position(self.x, self.y + self.height / 2 - top_border.height / 2)
 
         bottom_border = arcade.Sprite("sprite/bottom_border.png")
-        bottom_border.width = self.width
-        bottom_border.height = self.height / 2.5
+        bottom_border.scale = self.scale
         bottom_border.set_position(self.x, self.y - self.height / 2 + bottom_border.height / 2)
 
-        self.borderList.append(top_border)
-        self.borderList.append(bottom_border)
+        self.border_list.append(top_border)
+        self.border_list.append(bottom_border)
 
     def setup(self):
         self.__init_borders()
-
-        self.field.width = self.width
-        self.field.height = self.height
         self.field.set_position(self.x, self.y)
 
     def draw(self):
         self.field.draw()
-        self.borderList.draw()
+        self.border_list.draw()
+        self.ball.draw()
+        self.player.draw()
+
+    def update(self, delta_time):
+        self.ball.update(delta_time)
+        self.player.update(delta_time)
+
+        if arcade.check_for_collision(self.player.sprite, self.ball.sprite):
+            self.ball.change_speed(self.player.speed_x, self.player.speed_y)
--- main.py
+++ main.py
@@ -4,33 +4,43 @@ import football
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
-
 class MyGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        self.ball = arcade.Sprite("sprite/ball.png")
-        self.field = football.Field(300, 300, 500, 250)
+        self.field = football.Field(300, 300, 0.5)
 
         arcade.set_background_color(arcade.color.WHITE)
 
     def setup(self):
-        self.ball.center_x = 100
-        self.ball.center_y = 100
-
-        self.ball.width = 20
-        self.ball.height = 20
-
         self.field.setup()
 
     def on_draw(self):
         arcade.start_render()
         self.field.draw()
-        self.ball.draw()
 
     def update(self, delta_time):
-        self.ball.center_x += delta_time * 100
-        self.ball.center_y = self.ball.center_x ** 1.02
+        self.field.update(delta_time)
+
+    def on_key_release(self, key: int, modifiers: int):
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.field.player.speed_y = 0
+
+        if key == arcade.key.RIGHT or key == arcade.key.LEFT:
+            self.field.player.speed_x = 0
+
+    def on_key_press(self, key: int, modifiers: int):
+        if key == arcade.key.UP:
+            self.field.player.speed_y = 100
+
+        if key == arcade.key.DOWN:
+            self.field.player.speed_y = -100
+
+        if key == arcade.key.RIGHT:
+            self.field.player.speed_x = 100
+
+        if key == arcade.key.LEFT:
+            self.field.player.speed_x = -100
 
 
 game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
