ff0d4179d4f229b391b67515132847c523e410c6
olegfomenko <oleg.fomenko2002@gmail.com>
2020-10-18 01:25:44 +0300

    new field design

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MyGame(arcade.Window):
         super().__init__(width, height)
 
         self.ball = arcade.Sprite("sprite/ball.png")
-        self.field = football.Field(300, 300, 100, 50)
+        self.field = football.Field(300, 300, 500, 250)
 
         arcade.set_background_color(arcade.color.WHITE)
 
@@ -25,8 +25,8 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        self.ball.draw()
         self.field.draw()
+        self.ball.draw()
 
     def update(self, delta_time):
         self.ball.center_x += delta_time * 100
