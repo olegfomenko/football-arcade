f4faadb9553229d4daeda10bcc0288f5b0642fb7
olegfomenko <oleg.fomenko2002@gmail.com>
2020-10-17 22:40:58 +0300

    adding ball sprite and scene updating

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ import arcade
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 
-arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Example")
 
+class MyGame(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        self.ball = arcade.Sprite("sprite/ball.png")
+
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        self.ball.center_x = 100
+        self.ball.center_y = 100
+        self.ball.width = 20
+        self.ball.height = 20
+
+    def on_draw(self):
+        arcade.start_render()
+        self.ball.draw()
+
+    def update(self, delta_time):
+        self.ball.center_x += delta_time * 100
+        self.ball.center_y = self.ball.center_x ** 1.02
+
+game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+game.setup()
+game.on_draw()
 arcade.run()
