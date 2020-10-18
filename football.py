7f637e4eae1cd1bf9f2bd34ad8455e5bd6dd810a
olegfomenko <oleg.fomenko2002@gmail.com>
2020-10-18 04:29:41 +0300

    solving collision bugs

--- football.py
+++ football.py
@@ -97,6 +97,9 @@ class Field:
 
         self.players.append(self.player)
 
+        self.count_blue = 0
+        self.count_red = 0
+
     def __init_borders(self):
         border = arcade.Sprite("sprite/top.png")
         border.scale = self.scale
@@ -167,20 +170,53 @@ class Field:
 
         if arcade.check_for_collision(self.player.sprite, self.ball.sprite):
             self.ball.change_speed(self.player.speed_x, self.player.speed_y)
-            self.player.speed_x = -self.player.speed_x
-            self.player.speed_y = -self.player.speed_y
 
-        if len(self.ball.sprite.collides_with_list(self.y_border_list)) > 0:
-            self.ball.speed_y = -self.ball.speed_y
-            self.player.update(delta_time)
+            if self.player.x < self.ball.x:
+                self.player.speed_x = -abs(self.player.speed_x)
+                self.ball.speed_x = abs(self.player.speed_x)
+            else:
+                self.player.speed_x = abs(self.player.speed_x)
+                self.ball.speed_x = -abs(self.player.speed_x)
 
-        if len(self.ball.sprite.collides_with_list(self.x_border_list)) > 0:
-            self.ball.speed_x = -self.ball.speed_x
-            self.player.update(delta_time)
+            if self.player.y < self.ball.y:
+                self.player.speed_y = -abs(self.player.speed_y)
+                self.ball.speed_y = abs(self.player.speed_y)
+            else:
+                self.player.speed_y = abs(self.player.speed_y)
+                self.ball.speed_y = -abs(self.player.speed_y)
+
+
+        border_list = self.ball.sprite.collides_with_list(self.y_border_list)
+
+        if len(border_list) > 0:
+            border = border_list[0]
+
+            if border.center_y < self.ball.y:
+                self.ball.speed_y = abs(self.ball.speed_y)
+            else:
+                self.ball.speed_y = -abs(self.ball.speed_y)
+
+        border_list = self.ball.sprite.collides_with_list(self.x_border_list)
+
+        if len(border_list) > 0:
+            border = border_list[0]
+
+            if border.center_x < self.ball.x:
+                self.ball.speed_x = abs(self.ball.speed_x)
+            else:
+                self.ball.speed_x = -abs(self.ball.speed_x)
 
         for i in range(0, len(self.players)):
             self.check(self.players[i])
 
-        if self.ball.x < self.x - self.width / 2 or self.ball.x > self.x + self.width / 2:
+        if self.ball.x < self.x - self.width / 2:
             self.ball.to_default()
-            print("Goal!")
+            self.player.to_default()
+            ++self.count_blue
+            print("Goal by blue! Red : Blue", self.count_red, ":", self.count_blue)
+
+        if self.ball.x > self.x + self.width / 2:
+            self.ball.to_default()
+            self.player.to_default()
+            ++self.count_red
+            print("Goal by red! Red : Blue", self.count_red, ":", self.count_blue)
